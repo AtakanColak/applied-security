eed31efbe7aa4e3d52bf5f3f38357fb121bf9aab
Atakan Colak <atakancolak9897@gmail.com>
2019-01-30 15:25:08 +0000

    Dongle Properized

--- lab-1-1/board/dongle.c
+++ lab-1-1/board/dongle.c
@@ -10,6 +10,12 @@ uint8_t hexchartoi(char hex) {
   else return -1;
 }
 
+uint8_t read_byte() {
+  char A     = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  char B     = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  return ((hexchartoi(A) << 4) & hexchartoi(B));
+}
+
 char itohexchar(uint8_t n, int i) {
   int s = n;
   if(i) s = (n >> 4);
@@ -18,59 +24,52 @@ char itohexchar(uint8_t n, int i) {
   else        return s + 'A'- 10;
 }
 
+void write_byte(uint8_t byte) {
+  char A = itohexchar(byte, 1);
+  char B = itohexchar(byte, 0);
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, A);
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, B);
+}
+
 int octetstr_rd( uint8_t* r, int n_r) {
-
-  char size_A     = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
-  scale_gpio_wr( SCALE_GPIO_PIN_TRG, true  );
-  char size_B     = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  int size = read_byte();
   char semi_colon = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
-  //scale_gpio_wr( SCALE_GPIO_PIN_GPO, true  );
-  int size = (hexchartoi(size_A) << 4) + hexchartoi(size_B);
 
-  //if (semi_colon != ':')  return -1;
-  //if (size > n_r - 1)     return -1;
+  if (semi_colon != ':')  return -1;
+  if (size > n_r )        return -1;
 
-
-  for (int i = 0; i < size; ++i) {
-    char hex_A     = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
-    char hex_B     = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
-    r[i] = (hexchartoi(hex_A) << 4) + hexchartoi(hex_B);
-  }
+  for (int i = 0; i < size; ++i) 
+    r[i] = read_byte();
+  
   //char eol = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
   //if (!(eol == '\n' || eol == '\r')) return -1;
   return size;
 }
 
 void octetstr_wr( const uint8_t* x, int n_x ) {
-  char size_A = itohexchar(n_x, 1);
-  char size_B = itohexchar(n_x, 0);
-
-  scale_uart_wr(SCALE_UART_MODE_BLOCKING, size_A);
-  scale_uart_wr(SCALE_UART_MODE_BLOCKING, size_B);
+  write_byte(n_x);
   scale_uart_wr(SCALE_UART_MODE_BLOCKING, ':');
 
-  for (int i = n_x - 1; i >= 0; --i) {
-    size_A = itohexchar(x[i], 1);
-    size_B = itohexchar(x[i], 0);
-    scale_uart_wr(SCALE_UART_MODE_BLOCKING, size_A);
-    scale_uart_wr(SCALE_UART_MODE_BLOCKING, size_B);
-  }
+  for (int i = 0; i < n_x; ++i)
+    write_byte(x[n_x - 1 - i]);
+
   scale_uart_wr(SCALE_UART_MODE_BLOCKING, '\r');
 }
 
 int main( int argc, char* argv[] ) {
-  // initialise the development board, using the default configuration
-  if( !scale_init( &SCALE_CONF ) ) {
-    return -1;
-  }
+  if( !scale_init( &SCALE_CONF ) ) return -1;
 
   int max_size = 256;
   int cur_size = 0;
   uint8_t local_mem[max_size];
   while( true ) {
-      //scale_delay_ms(1000);
-
+      scale_delay_ms(1000);
+      scale_gpio_wr( SCALE_GPIO_PIN_TRG, true  );
       cur_size = octetstr_rd(local_mem, max_size);
+      if (cur_size == -1) {
+        scale_gpio_wr( SCALE_GPIO_PIN_GPO, true );
+        break;
+      }
       octetstr_wr(local_mem, cur_size);
       scale_gpio_wr( SCALE_GPIO_PIN_TRG, false );
       break;
@@ -78,52 +77,3 @@ int main( int argc, char* argv[] ) {
   return 0;
 }
 
-
-
-//   bool scale_uaint octetstr_rd( uint8_t* r, int n_r )rt_rd_avail()
-// Check if UART is available for read (i.e., would doing so block or not).
-// •
-// bool scale_uart_wr_avail()
-// Check if UART is available for write (i.e., would doing so block or not).
-// •
-// uint8_t scale_uart_rd( scale_uart_mode_t mode )
-// Perform a blocking
-// orint octetstr_rd( uint8_t* r, int n_r )
-// non-blocking (per
-// mode
-// ) read of an 8-bit byte from the UART, returning said byte
-// as the result.
-// •
-// void scale_uart_wr( scale_uart_mode_t mode, uint8_t x )
-// Perform a blocking
-// or
-// non-blocking (per
-// mode
-// ) write of an 8-bit byte
-// x
-// to the UART
-
-  // char x[] = "hello world";
-
-  // while( true ) {
-  //   // read  the GPI     pin, and hence switch : t   <- GPI
-  //   bool t = scale_gpio_rd( SCALE_GPIO_PIN_GPI        );
-  //   // write the GPO     pin, and hence LED    : GPO <- t
-  //            scale_gpio_wr( SCALE_GPIO_PIN_GPO, t     );
-  //
-  //   // write the trigger pin, and hence LED    : TRG <- 1 (positive edge)
-  //            scale_gpio_wr( SCALE_GPIO_PIN_TRG, true  );
-  //   // delay for 500 ms = 1/2 s
-  //   scale_delay_ms( 500 );
-  //   // write the trigger pin, and hence LED    : TRG <- 0 (negative edge)
-  //            scale_gpio_wr( SCALE_GPIO_PIN_TRG, false );
-  //   // delay for 500 ms = 1/2 s
-  //   scale_delay_ms( 500 );
-  //
-  //   int n = strlen( x );
-  //
-  //   // write x = "hello world" to the UART
-  //   for( int i = 0; i < n; i++ ) {
-  //     scale_uart_wr( SCALE_UART_MODE_BLOCKING, x[ i ] );
-  //   }
-  // }

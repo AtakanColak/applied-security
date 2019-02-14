807c376531604b49f9a3c3b8be57115bae2fe592
Atakan Colak <atakancolak9897@gmail.com>
2019-02-14 11:23:54 +0000

    Random Seed

--- lab-3/atarsa2.c
+++ lab-3/atarsa2.c
@@ -1,24 +1,43 @@
-#include "atarsa.h"
- 
+#include "atarsa2.h"
+
 int main(int argc, char *argv[])
 {
-    mpz_t a,b,c;
-    rsa_keygen(a,b,c, 1024);
+    
+    atarsa_primegen(1024);
+    
     return 0;
 }
 
-void rsa_keygen( mpz_t N, mpz_t e, mpz_t d, int lambda ) {
-    mpz_t p;
+void rsa_keygen(mpz_t N, mpz_t e, mpz_t d, int lambda)
+{
+}
+
+void atarsa_primegen(int lambda)
+{
+    mpz_t p, seed;
+    mp_limb_t seed_l = (rand() << 32) | (rand());
     mpz_init(p);
-   
-}
+    mpz_init(seed);
+    mpz_import(seed, NULL, -1, sizeof(seed_l), -1, 0, seed_l);
+    gmp_randstate_t state;
+    gmp_randinit_default(state);
+    gmp_randseed(state, seed);
+    mpz_urandomb(p, state, lambda);
+    size_t n = mpz_size(p);
+    mp_limb_t t[n];
 
-void atarsa_primegen(mpz_t p, int lambda) {
-    mpz_urandomb(p, gmp_randinit_mt, lambda);
-    size_t n = mpz_size( p );
-    mp_limb_t t[ n ];
-    mpz_export(t, NULL, 1, sizeof(mp_limb_t), 1, 0, p);
-    for(int i = 0; i < n; ++i)
-        gmp_printf("t[%d]\t : %d\t\n", i, t[i]);
+    mpz_export(t, NULL, -1, sizeof(mp_limb_t), -1, 0, p);
+    //for (int i = 0; i < n; ++i)
+        //gmp_printf("t[%d]\t : %d\t\n", i, t[i]);
+    //Make sure it is odd
+    t[0] |= 1;
+    //Make sure big enough
+    t[n-1] |= ((unsigned long) 1) << (((sizeof(mp_limb_t) - 1) * 8) + 7);
+    for (int i = 0; i < n; i++)
+    {
+        gmp_printf("t[%d] : %llu*(2^(64))^(%d)\n", i, t[i], i);
+    }
+    gmp_printf( "\n" );
+    gmp_randclear(state);
+    mpz_clear(p);
 }
-
--- lab-3/atarsa2.h
+++ lab-3/atarsa2.h
@@ -12,13 +12,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #include <gmp.h>
 
 // #define MAX(x, y) ({x > y ? x : y;})
 // #define MIN(x, y) ({x > y ? y : x;})
 
-void atarsa_primegen(mpz_t p, int lambda);
+void atarsa_primegen(int lambda);
 void rsa_keygen( mpz_t N, mpz_t e, mpz_t d, int lambda);
 
 #endif
